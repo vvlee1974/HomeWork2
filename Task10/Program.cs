6b33a7013f543e842c9cc21352b32c8369239230
vvlee <vvlee@gmail.com>
2022-12-19 19:12:41 +0500

    add Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*
+Алгоритм
+Ввести трёхзначное число с проверкой  от "балбеса"
+Произвести деление на 10 без остатка (/)
+Результат разделить на 10 с остатком (%)
+Вывести результат в консоли
+*/
+
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число!");
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+//string NumberStr = Console.ReadLine() ?? "";
+int Number = GetNumberFromConsole("Введите трёхзначное число: ");
+
+if(Number >= 100 && Number < 1000)
+{
+    int num = Number / 10;
+    int num2 = num % 10;
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число!");
+}
